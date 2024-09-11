8d8197db9b6c92f01b7d323a92ea01ea80141dc1
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2024-09-11 13:19:49 +0200

    code updated

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -3,33 +3,33 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://blog-server-v2u7.onrender.com/api/auth",
-    credentials: "include",
+    baseUrl: "https://blog-server-v2u7.onrender.com/api/auth", // Adjusted to include /api/auth
+    credentials: "include",  // Ensures cookies are included in the requests
   }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (newUser) => ({
-        url: "/register",
+        url: "/register", // will call /api/auth/register
         method: "POST",
         body: newUser,
       }),
     }),
     loginUser: builder.mutation({
       query: (credentials) => ({
-        url: "/login",
+        url: "/login", // will call /api/auth/login
         method: "POST",
         body: credentials,
       }),
     }),
     logoutUser: builder.mutation({
       query: () => ({
-        url: "/logout",
+        url: "/logout", // will call /api/auth/logout
         method: "POST",
       }),
     }),
     getUsers: builder.query({
       query: () => ({
-        url: "/users",
+        url: "/users", // will call /api/auth/users
         method: "GET",
       }),
       refetchOnMount: true,
@@ -37,14 +37,13 @@ const authApi = createApi({
     }),
     deleteUser: builder.mutation({
       query: (userId) => ({
-        url: `/users/${userId}`,
+        url: `/users/${userId}`, // will call /api/auth/users/:userId
         method: "DELETE",
       }),
     }),
-
     updateUserRole: builder.mutation({
       query: ({ userId, role }) => ({
-        url: `/users/${userId}`,
+        url: `/users/${userId}`, // will call /api/auth/users/:userId
         method: "PUT",
         body: { role },
       }),
